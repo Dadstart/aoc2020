7af8e42dd6922fd9ecb1c8abf17fa129449e1f86
Andrew Bishop <andrew@bishop4.com>
2020-12-05 17:57:21 -0800

    Part 1 solved

---
+++ src/day5/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+
+namespace day5
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// day 1
+			TestGetSeat("FBFBBFFRLR",  44, 5, 357);
+			TestGetSeat("BFFFBBFRRR",  70, 7, 567);
+			TestGetSeat("FFFBBBFRRR",  14, 7, 119);
+			TestGetSeat("BBFFBBFRLL", 102, 4, 820);
+
+			var highestSeat = 0;
+			using (var reader = new StreamReader(@"..\..\..\input.txt"))
+			{
+				while (!reader.EndOfStream)
+				{
+					var line = reader.ReadLine();
+					if (line.Length == 0)
+						continue;
+
+					var seat = GetSeat(reader.ReadLine());
+					if (seat.id > highestSeat)
+						highestSeat = seat.id;
+				}
+			}
+
+			Console.WriteLine($"Highest seat id: {highestSeat}");
+		}
+
+		static void TestGetSeat(string input, int row, int col, int id)
+		{
+			// I should really start using xUnit
+			var seat = GetSeat(input);
+			Test.AssertEqual(seat.row, row);
+			Test.AssertEqual(seat.col, col);
+			Test.AssertEqual(seat.id, id);
+		}
+
+		static (int row, int col, int id) GetSeat(string input)
+		{
+			int row = -1;
+			int col = -1;
+			int id = -1;
+
+			row = GetLocation(input.Substring(0, 7), 'F', 'B', 0, 127);
+			col = GetLocation(input.Substring(7, 3), 'L', 'R', 0, 7);
+			id = row * 8 + col;
+
+			return (row, col, id);
+		}
+
+		static int GetLocation(string input, char lower, char higher, int min, int max)
+		{
+			foreach (char ch in input)
+			{
+				if (ch == lower)
+				{
+					max = ((max - min) / 2) + min;
+				}
+				else if (ch == higher)
+				{
+					min = (max - min) / 2 + min + 1;
+				}
+				else
+				{
+					throw new InvalidOperationException($"Invalid char '{ch}'");
+				}
+			}
+
+			if (min != max)
+				throw new InvalidOperationException($"Did not find value");
+
+			return min;
+		}
+	}
+}
---
+++ src/day5/Test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace day5
+{
+	public static class Test
+	{
+		public static void AssertEqual(int val, int expected)
+		{
+			if (val != expected)
+				throw new Exception($"Expected {expected}; Actual {val}");
+		}
+	}
+}
