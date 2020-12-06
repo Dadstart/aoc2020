d56c91e48fce4babbdceaf304d71ba2e552c2abe
Andrew Bishop <andrew@bishop4.com>
2020-12-06 12:20:25 -0800

    Day 7 ready

---
+++ src/day7/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace day7
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// day 1
+			//const string inputFile = @"..\..\..\sample-input.txt";
+			const string inputFile = @"..\..\..\input.txt";
+			using (var reader = new StreamReader(inputFile))
+			{
+				while (!reader.EndOfStream)
+				{
+
+				}
+			}
+		}
+	}
+}
