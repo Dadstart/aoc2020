408307703102f9d575a1613893de73d853a43eef
Andrew Bishop <andrew@bishop4.com>
2020-12-07 13:04:15 -0800

    Day8 project added

---
+++ src/day8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace day8
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// day 1
+			const string inputFile = @"..\..\..\sample-input.txt";
+			//const string inputFile = @"..\..\..\input.txt";
+			using (var reader = new StreamReader(inputFile))
+			{
+				while (!reader.EndOfStream)
+				{
+					var line = reader.ReadLine();
+					if (line.Length == 0)
+						continue;
+				}
+			}
+		}
+	}
+}
