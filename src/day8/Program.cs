a934bb12449cb7340fba16a15e36cd9faf397513
Andrew Bishop <andrew@bishop4.com>
2020-12-08 12:39:18 -0800

    Part 1

--- src/day8/Program.cs
+++ src/day8/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace day8
 {
@@ -7,9 +10,10 @@ namespace day8
 	{
 		static void Main(string[] args)
 		{
+			var instructions = new List<Instruction>();
 			// day 1
-			const string inputFile = @"..\..\..\sample-input.txt";
-			//const string inputFile = @"..\..\..\input.txt";
+			//const string inputFile = @"..\..\..\sample-input.txt";
+			const string inputFile = @"..\..\..\input.txt";
 			using (var reader = new StreamReader(inputFile))
 			{
 				while (!reader.EndOfStream)
@@ -17,8 +21,59 @@ namespace day8
 					var line = reader.ReadLine();
 					if (line.Length == 0)
 						continue;
+
+					var match = Regex.Matches(line, @"\b(?<op>\w\w\w)\s(?<argSign>[+-])(?<argNum>\d+)").First();
+
+					InstructionType type;
+					switch (match.Groups["op"].Value)
+					{
+						case "nop":
+							type = InstructionType.Nop;
+							break;
+						case "jmp":
+							type = InstructionType.Jmp;
+							break;
+						case "acc":
+							type = InstructionType.Acc;
+							break;
+						default:
+							throw new InvalidOperationException($"Unknown instruction type {match.Groups["op"].Value}");
+					}
+
+					var arg = int.Parse(match.Groups["argNum"].Value);
+					if (match.Groups["argSign"].Value == "-")
+						arg *= -1;
+
+					instructions.Add(new Instruction(type, arg));
 				}
 			}
+
+			int i = 0;
+			int acc = 0;
+			while (i < instructions.Count)
+			{
+				var instr = instructions[i];
+				if (instr.ExecutionCount > 0)
+					break;
+
+				switch (instr.Type)
+				{
+					case InstructionType.Acc:
+						acc += instr.Arg;
+						i++;
+						break;
+					case InstructionType.Jmp:
+						i += instr.Arg;
+						break;
+					case InstructionType.Nop:
+						i++;
+						break;
+				}
+
+				instr.ExecutionCount++;
+			}
+
+			Console.WriteLine($"Halted at instruction {i}. Accumulator value {acc}");
 		}
 	}
 }
