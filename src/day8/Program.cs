3ea11feafb616fff78be2ad5b508fd76a9e1299c
Andrew Bishop <andrew@bishop4.com>
2020-12-08 13:20:44 -0800

    Part 2

--- src/day8/Program.cs
+++ src/day8/Program.cs
@@ -74,6 +74,77 @@ namespace day8
 			}
 
 			Console.WriteLine($"Halted at instruction {i}. Accumulator value {acc}");
+
+			// part 2
+
+			// brute force this
+			bool success = false;
+			for (i = 0; i < instructions.Count; i++)
+			{
+				// reset execution count
+				foreach (var instr in instructions)
+					instr.ExecutionCount = 0;
+
+				if (TryTerminateLoop(instructions, i, out acc))
+				{
+					success = true;
+					break;
+				}
+			}
+
+			if (success)
+				Console.WriteLine($"Changed instruction {i}. Accumulator value {acc}");
+			else
+				Console.WriteLine("Failed to terminate loop");
+
+		}
+
+		static bool TryTerminateLoop(List<Instruction> instructions, int opToChange, out int acc)
+		{
+			acc = 0;
+
+			var origInstr = instructions[opToChange];
+			Instruction newInstr;
+			var instrToChange = instructions[opToChange];
+			if (instrToChange.Type == InstructionType.Jmp)
+				newInstr = new Instruction(InstructionType.Nop, origInstr.Arg);
+			else if (instrToChange.Type == InstructionType.Nop)
+				newInstr = new Instruction(InstructionType.Jmp, origInstr.Arg);
+			else
+				return false;
+
+			instructions[opToChange] = newInstr;
+
+			bool halted = false;
+			int i = 0;
+			while (i < instructions.Count)
+			{
+				var instr = instructions[i];
+				if (instr.ExecutionCount > 0)
+				{
+					halted = true;
+					break;
+				}
+
+				switch (instr.Type)
+				{
+					case InstructionType.Acc:
+						acc += instr.Arg;
+						i++;
+						break;
+					case InstructionType.Jmp:
+						i += instr.Arg;
+						break;
+					case InstructionType.Nop:
+						i++;
+						break;
+				}
+
+				instr.ExecutionCount++;
+			}
+
+			instructions[opToChange] = origInstr;
+			return !halted;
 		}
 	}
 }
