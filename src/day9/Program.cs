c350b1f50fbd45c52362501942e5e63311d63b98
Andrew Bishop <andrew@bishop4.com>
2020-12-08 13:23:35 -0800

    Day 9 project

---
+++ src/day9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace day9
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// part 1
+			const string inputFile = @"..\..\..\sample-input.txt";
+			//const string inputFile = @"..\..\..\input.txt";
+			using (var reader = new StreamReader(inputFile))
+			{
+				while (!reader.EndOfStream)
+				{
+					var line = reader.ReadLine();
+					if (line.Length == 0)
+						continue;
+
+				}
+			}
+		}
+	}
+}
