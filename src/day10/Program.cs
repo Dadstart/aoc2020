f7a48e9d3abfb1679c753670d7acb2a49ab5c556
Andrew Bishop <andrew@bishop4.com>
2020-12-10 15:42:49 -0800

    Part 1 solved

--- src/day10/Program.cs
+++ src/day10/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace day10
@@ -8,8 +9,9 @@ namespace day10
 		static void Main(string[] args)
 		{
 			// part 1
-			const string inputFile = @"..\..\..\sample-input.txt";
-			//const string inputFile = @"..\..\..\input.txt";
+			var adapters = new List<int>();
+			//const string inputFile = @"..\..\..\sample-input2.txt";
+			const string inputFile = @"..\..\..\input.txt";
 			using (var reader = new StreamReader(inputFile))
 			{
 				while (!reader.EndOfStream)
@@ -18,8 +20,27 @@ namespace day10
 					if (line.Length == 0)
 						continue;
 
+					adapters.Add(int.Parse(line));
 				}
 			}
+
+			var voltDiffs = new int[] { 0, 0, 0, 0};
+			adapters.Sort();
+
+			// add outlet of 0 jolts
+			adapters.Insert(0, 0);
+
+			// add built-in adapter of +3 jolts
+			adapters.Add(adapters[adapters.Count - 1] + 3);
+
+			for (int i = 1; i < adapters.Count; i++)
+			{
+				var diff = adapters[i] - adapters[i - 1];
+				voltDiffs[diff]++;
+			}
+
+			Console.WriteLine($"1-volt diffs: {voltDiffs[1]}; 3-volt diffs: {voltDiffs[3]}");
+			Console.WriteLine($"Product is {voltDiffs[1] * voltDiffs[3]}");
 		}
 	}
 }
