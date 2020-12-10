d08b18b8ed72cb8b08d40b5c00ec058fdaad46ae
Andrew Bishop <andrew@bishop4.com>
2020-12-10 10:33:32 -0800

    Part 1

--- src/day9/Program.cs
+++ src/day9/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace day9
@@ -8,8 +9,9 @@ namespace day9
 		static void Main(string[] args)
 		{
 			// part 1
-			const string inputFile = @"..\..\..\sample-input.txt";
-			//const string inputFile = @"..\..\..\input.txt";
+			var nums = new List<ulong>();
+			//const string inputFile = @"..\..\..\sample-input.txt";
+			const string inputFile = @"..\..\..\input.txt";
 			using (var reader = new StreamReader(inputFile))
 			{
 				while (!reader.EndOfStream)
@@ -18,8 +20,38 @@ namespace day9
 					if (line.Length == 0)
 						continue;
 
+					nums.Add(ulong.Parse(line));
 				}
 			}
+
+			const int prevNumCount = 25;
+			ulong invalidNum = 0;
+			for (int pos = prevNumCount; pos < nums.Count; pos++)
+			{
+				bool found = false;
+				for (int i = pos - prevNumCount; i < pos; i++)
+				{
+					for (int j = i + 1; j < pos; j++)
+					{
+						if (nums[i] + nums[j] == nums[pos])
+						{
+							found = true;
+							break;
+						}
+					}
+
+					if (found)
+						break;
+				}
+
+				if (!found)
+				{
+					invalidNum = nums[pos];
+					break;
+				}
+			}
+
+			Console.WriteLine($"First invalid number is {invalidNum}");
 		}
 	}
 }
